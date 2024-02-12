897b7ec6b9eeef70b413b2b12ec20960790cded5
Kevin Russell <kevinfrussell@gmail.com>
2024-02-12 16:10:01 -0500

    Intake incorporated with Shooter command example

--- src/main/java/frc/robot/commands/ShooterVelocityCommand.java
+++ src/main/java/frc/robot/commands/ShooterVelocityCommand.java
@@ -5,10 +5,13 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 
 public class ShooterVelocityCommand extends Command {
   private ShooterSubsystem m_shooter;
+  // private IntakeSubsystem m_intake;
+  private int m_ticks;
   private ShooterSubsystem.ShooterLocation m_location;
 
   /** Creates a new ShooterVelocityComma`nd. */
@@ -26,16 +29,23 @@ public class ShooterVelocityCommand extends Command {
   @Override
   public void initialize() {
     m_shooter.controlShooterToVelocity(m_location);
+    m_ticks = 0;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    if (++m_ticks == 12)
+    {
+      // m_intake.intake();
+    }
+  }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     m_shooter.stopShooterMotor();
+    // m_intake.stopMotor();
   }
 
   // Returns true when the command should end.

801d5f0a01718ba082e2a92b86e15dfcb6a322b3
Kevin Russell <kevinfrussell@gmail.com>
2024-03-20 09:31:56 -0400

    Clean out unused

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -1,30 +1,8 @@
 package frc.robot.subsystems;
 
-import cwtech.util.AprilTagNav;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Vision {
-    public AprilTagNav m_nav;
-
-    public AprilTagNav getNav() {
-        return m_nav;
-    }
-
-    // static NetworkTableEntry m_botposeEntry = NetworkTableInstance.getDefault().getTable("limelight-cwtech")
-    //         .getEntry("botpose");
-    // static NetworkTableEntry m_pipelineEntry = NetworkTableInstance.getDefault().getTable("limelight-cwtech")
-    //         .getEntry("pipeline");
-    // static NetworkTableEntry m_closestAprilTag = NetworkTableInstance.getDefault().getTable("limelight-cwtech")
-    //         .getEntry("tid");
-
-    public Vision() {
-        try {
-            m_nav = new AprilTagNav();
-        } catch(Exception e) {
-            DriverStation.reportError(e.getMessage(), e.getStackTrace());
-        }
-    }
 
     public static final String kLimeLightName = "limelight";
 

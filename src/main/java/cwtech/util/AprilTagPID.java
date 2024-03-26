21de96679753a3bf21901874f5f545a855b2f197
CW Tech <cwtech2959@gmail.com>
2024-03-26 19:58:43 -0400

    Work on April Tag PID class

--- src/main/java/cwtech/util/AprilTagPID.java
+++ src/main/java/cwtech/util/AprilTagPID.java
@@ -6,24 +6,29 @@ package cwtech.util;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.LimelightHelpers;
+import frc.robot.subsystems.Vision;
 
 /** Add your docs here. */
 public class AprilTagPID
 {
+    private DriveSubsystem m_driveSubsystem;
     private double kSpeedKp = 0.38;
     private double kRotationKp = 0.035;
     private double kSpeedKi = 0;
     private double kRotationKi = 0;
     private double kSpeedKd = 0;
     private double kRotationKd = 0;
- 
 
     PIDController m_rotationController = new PIDController(kRotationKp, kRotationKi, kRotationKd);
     PIDController m_xSpeedController = new PIDController(kSpeedKp, kSpeedKi, kSpeedKd);
-    PIDController m_ySpeedController = new PIDController(kSpeedKp, kSpeedKi, kSpeedKd);
+    PIDController m_zSpeedController = new PIDController(kSpeedKp, kSpeedKi, kSpeedKd);
 
-    public AprilTagPID()
+    public AprilTagPID(DriveSubsystem driveSubsystem)
     {
+        m_driveSubsystem = driveSubsystem;
+
         SmartDashboard.putNumber("AprilTag/kP Speed", kSpeedKp);
         SmartDashboard.putNumber("AprilTag/kP Rotation", kRotationKp);
         SmartDashboard.putNumber("AprilTag/kI Speed", kSpeedKi);
@@ -41,15 +46,51 @@ public class AprilTagPID
         kSpeedKd = SmartDashboard.getNumber("AprilTag/kD Speed", kSpeedKd);
         kRotationKd = SmartDashboard.getNumber("AprilTag/kD Rotation", kRotationKd);
     }
+
+    public void setTargetPosition(double xPosition, double zPosition, double rotation)
+    {
+        m_xSpeedController.setSetpoint(xPosition);
+        m_zSpeedController.setSetpoint(zPosition);
+        m_rotationController.setSetpoint(rotation);
+    }
+
+    private double zSpeed(double tz)
+    {
+        double targetingForwardSpeed = m_zSpeedController.calculate(tz);
+        targetingForwardSpeed *= DriveSubsystem.kMaxSpeedMetersPerSecond;
+        return targetingForwardSpeed;
+    }
+
+    private double ySpeed(double tx)
+    {
+        double targetingForwardSpeed = m_xSpeedController.calculate(tx);
+        targetingForwardSpeed *= DriveSubsystem.kMaxSpeedMetersPerSecond;
+        targetingForwardSpeed *= -1.0;
+        return targetingForwardSpeed;
+    }
+
+    private double rotationTarget()
+    {
+        double botAngle = m_driveSubsystem.getAngle().getDegrees();
+        double remappedAngle = remapAngle(botAngle);
+        double targetingAngularVelocity = m_rotationController.calculate(remappedAngle);
+        targetingAngularVelocity *= DriveSubsystem.kMaxAngularSpeedRadiansPerSecond;
+
+        return targetingAngularVelocity;
+    }
+
+    public void driveToTarget(double tx, double tz)
+    {
+        m_driveSubsystem.drive(-zSpeed(tz), ySpeed(tx), rotationTarget(), false);
+    }
+
+    private double remapAngle(double fromNavX)
+    {
+        double remapAngle = fromNavX % 360;
+        if (remapAngle <= -45) 
+        {
+            remapAngle = remapAngle + 360;
+        }
+        return remapAngle;
+    }
 }
-  // from 2023 arm rotation subsystem
-  // private ProfiledPIDController m_armRotatorMotorProfiledPidController = new ProfiledPIDController(kArmRotatorP,
-  //     kArmRotatorI, kArmRotatorD,
-  //     new Constraints(kArmRotatorMaxVelocity, kArmRotatorMaxAccel));
-
-  // double rawProfiled = m_armRotatorMotorProfiledPidController.calculate(getArmAngle());
-  //     m_armRotatorMotor.set(rawProfiled);
-
-  // // profiled PID control
-  //   m_armRotatorMotorProfiledPidController.setGoal(degrees);
-
